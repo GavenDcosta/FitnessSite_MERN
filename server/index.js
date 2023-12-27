c992c53cece21274a7faa22f7cd3576959b4d824
Gaven Dcosta <gavendcosta@gmail.com>
2023-12-27 08:03:49 +0530

    schedule fetching and updating

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 import axios from 'axios';
 import Exercise from './models/exerciseModel.js'
 import exerciseRoutes from './routes/exerciseRoutes.js'
-
+import schedule from 'node-schedule'
 
 dotenv.config()
 
@@ -64,37 +64,16 @@ async function fetchAndUpdateDB(){
 
 
 
-function scheduleDailyTask(hour, minute, callback) {
-  const now = new Date();
-  const targetTime = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate(),
-    hour,
-    minute,
-    0,
-    0
-  );
+// Schedule the function to run every day at 8 AM IST
+const rule = new schedule.RecurrenceRule();
+rule.hour = 23;
+rule.minute = 35;
+rule.second = 0;
+rule.tz = 'Asia/Kolkata'; // Set the timezone to IST
 
-  // Adjust for IST (UTC+5:30)
-  targetTime.setUTCHours(targetTime.getUTCHours() + 5);
-  targetTime.setUTCMinutes(targetTime.getUTCMinutes() + 30);
+schedule.scheduleJob(rule, async () => {
+  console.log('Running fetchAndUpdateDB at 8 AM IST...');
+  await fetchAndUpdateDB();
+});
 
-  // If the target time has already passed, set it for the next day
-  if (now > targetTime) {
-    targetTime.setDate(targetTime.getDate() + 1);
-  }
-
-  const timeDifference = targetTime - now;
-
-  // Set interval to run the task every 24 hours
-  setInterval(callback, 24 * 60 * 60 * 1000);
-
-  // Set timeout for the first run
-  setTimeout(() => {
-    callback();
-  }, timeDifference);
-}
-
-// Schedule the task to run every day at 11:32 pm IST
-scheduleDailyTask(23, 32, fetchAndUpdateDB);
+console.log('Scheduled job to run every day at 8 AM IST.');
