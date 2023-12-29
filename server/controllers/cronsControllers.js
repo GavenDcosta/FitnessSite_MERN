6a74fd596a419f561ea771052c32c617cf700f89
Gaven Dcosta <gavendcosta@gmail.com>
2023-12-29 14:58:35 +0530

    res changes

--- server/controllers/cronsControllers.js
+++ server/controllers/cronsControllers.js
@@ -7,33 +7,41 @@ import Exercise from '../models/exerciseModel.js';
 
 export const fetchAndUpdateDB = async (req, res) => {
 
-  const RAPIDAPI_KEY = process.env.RAPIDAPI_KEY
+  try {
+    const RAPIDAPI_KEY = process.env.RAPIDAPI_KEY
 
-  const response = await axios.request({
-    method: 'GET',
-    url: 'https://exercisedb.p.rapidapi.com/exercises',
-    params: {limit: '1400'},
-    headers: {
-      'X-RapidAPI-Key': RAPIDAPI_KEY,
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    },
-  });
-
-  const exercisesData = response.data;
-
-  
-  Exercise.deleteMany({})
-    .then(() => {
-      console.log('Old exercises deleted successfully.');
-  
-      return Exercise.insertMany(exercisesData);
-    })
-    .then((exercises) => {
-      console.log('New exercises inserted successfully:', exercises);
-    })
-    .catch((err) => {
-      console.error(err.message);
+    const response = await axios.request({
+      method: 'GET',
+      url: 'https://exercisedb.p.rapidapi.com/exercises',
+      params: {limit: '1400'},
+      headers: {
+        'X-RapidAPI-Key': RAPIDAPI_KEY,
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      },
     });
+  
+    const exercisesData = response.data;
+  
+    
+    Exercise.deleteMany({})
+      .then(() => {
+        console.log('Old exercises deleted successfully.');
+    
+        return Exercise.insertMany(exercisesData);
+      })
+      .then((exercises) => {
+        console.log('New exercises inserted successfully:', exercises);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+
+      res.send("Data updated successfully : ", exercisesData)
+
+    } catch (error) {
+      res.status(404).json({message: error})
+  }
+
 }
 
 
